b62f0fee082e4ae7cc890c8ea15fdac433302b53
Arunan Gnanasekaran <arunan2212@gmail.com>
2023-11-24 00:25:31 +0100

    Add bottom bar

---
+++ app/(tabs)/create-board.tsx
@@ -0,0 +1,83 @@
+import { StyleSheet } from "react-native";
+
+import { View } from "../../components/Themed";
+import SudokuGrid from "../../components/SudokuGrid";
+import InputControls from "../../components/Controls";
+import { useState } from "react";
+import { difficulties } from "../../types/Difficulty";
+import { StoredBoard, SudokuBoard } from "../../types/board";
+import { useStoredBoards } from "../../hooks/sudoku/useStoredBoards";
+import { copyBoard, createEmptyBoard, createEmptyNumberBoard } from "../../hooks/sudoku/sudoku";
+import useSudoku from "../../hooks/sudoku/useSudoku";
+import SudokuTopBar from "../../components/SudokuTopBar";
+import SudokuBottomBar from "../../components/SudokuBottomBar";
+
+export default function TabTwoScreen() {
+  const [board] = useState<StoredBoard>({
+    board: createEmptyNumberBoard(),
+    solvedBoard: createEmptyNumberBoard(-1),
+    difficulty: difficulties.EASY,
+  } as StoredBoard);
+
+  const sudoku = useSudoku(board);
+
+  const [_, createNewBoard] =  useStoredBoards();
+
+  const [difficulty, setDifficulty] = useState<string>(difficulties.EASY);
+
+  function handleOnCreate() {
+    console.log(difficulty)
+    if (boardIsEmpty()) {
+      alert("Board is empty");
+      return;
+    }
+    try {
+      createNewBoard(sudoku.board, difficulty);
+    } catch (e) {
+      alert(e);
+    }
+  }
+
+  function boardIsEmpty() {
+    if (!sudoku.board) {
+      return true;
+    }
+    return sudoku.board.every((row) => {
+      return row.every((cell) => {
+        return cell.value === 0;
+      });
+    });
+  }
+
+  return (
+    <View style={styles.container}>
+      <SudokuTopBar difficulty={difficulty} onPickDifficulty={setDifficulty} />
+      <SudokuGrid
+        grid={sudoku.board}
+        selectedCell={sudoku.selectedCell}
+        onCellClick={sudoku.changeSelectedCell}
+      />
+      <InputControls
+        onClick={sudoku.setNewValueOnSelectedCell}
+      />
+      <SudokuBottomBar onClear={() => sudoku.setNewValueOnSelectedCell(0)} onCreate={handleOnCreate} creatorMode />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
--- components/Controls.tsx
+++ components/Controls.tsx
@@ -1,46 +1,13 @@
-import { Pressable, StyleSheet } from "react-native";
-import Button from "./Button";
+import { StyleSheet } from "react-native";
 import { Text, View } from "./Themed";
 import { TouchableOpacity } from "react-native-gesture-handler";
-import { Picker } from "@react-native-picker/picker";
-import Difficulty, { difficulties } from "../types/Difficulty";
-import { useTranslation } from "react-i18next";
 
 interface props {
   onClick: (num: number) => void;
-  onClear: () => void;
-  onSolve: () => void;
-  onCreate?: () => void;
-  onMark?: () => void;
-  creatorMode?: boolean;
 }
-export default function InputControls({
-  onClick,
-  onSolve,
-  onClear,
-  onCreate,
-  onMark,
-  creatorMode,
-}: props) {
+export default function InputControls(props: props) {
   const gridSize = 3;
 
-  const { t } = useTranslation();
-
-  function generateSudokuHTMLGrid() {
-    const htmlGrid = [];
-    for (let row = 0; row < gridSize; row++) {
-      const rowCells = [];
-      for (let col = 0; col < gridSize; col++) {
-        const num = gridSize ** 2 - (row * gridSize + col);
-      }
-      htmlGrid.push(
-        <View key={`row-${row}`} style={styles.gridRow}>
-          {rowCells}
-        </View>
-      );
-    }
-    return htmlGrid;
-  }
 
   const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -51,7 +18,7 @@ export default function InputControls({
           return (
             <TouchableOpacity
               key={num}
-              onPress={() => onClick(num)}
+              onPress={() => props.onClick(num)}
               style={styles.cell}
             >
               <Text>{num}</Text>
@@ -59,34 +26,6 @@ export default function InputControls({
           );
         })}
       </View>
-      <Button
-        text={t("solve")}
-        onPress={() => {
-          if (onSolve) onSolve();
-        }}
-      />
-      <Button
-        text={t("clear")}
-        onPress={() => {
-          if (onClear) onClear();
-        }}
-      />
-      <Button
-        text={t("mark")}
-        onPress={() => {
-          if (onMark) onMark();
-        }}
-      />
-      {creatorMode && (
-        <>
-          <Button
-            text={t("create")}
-            onPress={() => {
-              if (onCreate) onCreate();
-            }}
-          />      
-        </>
-      )}
     </>
   );
 }
