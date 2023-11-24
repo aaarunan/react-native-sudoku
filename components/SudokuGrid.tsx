8e4ef8751a17bd5d7cfdd0a2e8cb13dc3a19915f
Arunan Gnanasekaran <arunan2212@gmail.com>
2023-11-24 14:34:46 +0100

    Make seperate SudokuCell class

--- components/SudokuGrid.tsx
+++ components/SudokuGrid.tsx
@@ -1,92 +1,73 @@
-import React, {useEffect, useState} from "react";
-import {Text, View} from "../components/Themed";
-import {StyleSheet} from "react-native";
-import {TouchableOpacity} from "react-native-gesture-handler";
-import {SudokuBoard} from "../types/board";
+import React, { useEffect, useState } from "react";
+import { Text, View } from "../components/Themed";
+import { StyleSheet } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { SudokuBoard } from "../types/board";
+import SudokuCell from "./SudokuCell";
 
 interface props {
-    grid: SudokuBoard | undefined;
-    selectedCell: [number, number];
-    onCellClick: (row: number, col: number) => void;
+  grid: SudokuBoard | undefined;
+  selectedCell: [number, number];
+  onCellClick: (row: number, col: number) => void;
 }
 
 export default function SudokuGrid(props: props) {
-    const GRIDSIZE = 9;
+  const GRIDSIZE = 9;
 
-    const [htmlGrid, setHTMLGrid] = useState(generateSudokuHTMLGrid());
+  const [htmlGrid, setHTMLGrid] = useState(generateSudokuHTMLGrid());
 
-    useEffect(() => {
-        setHTMLGrid(generateSudokuHTMLGrid());
-    }, [props.grid, props.selectedCell]);
+  useEffect(() => {
+    setHTMLGrid(generateSudokuHTMLGrid());
+  }, [props.grid, props.selectedCell]);
 
-    function generateSudokuHTMLGrid(
-        sudokuGrid: SudokuBoard | undefined = props.grid,
-        gridSize = GRIDSIZE
-    ): JSX.Element[] {
-        if (sudokuGrid === undefined) {
-            return [];
+  function generateSudokuHTMLGrid(
+    sudokuGrid: SudokuBoard | undefined = props.grid,
+    gridSize = GRIDSIZE
+  ): JSX.Element[] {
+    if (sudokuGrid === undefined) {
+      return [];
+    }
+    const htmlGrid = [];
+    for (let row = 0; row < gridSize; row++) {
+      const rowCells = [];
+      for (let col = 0; col < gridSize; col++) {
+        let val = sudokuGrid[row][col].value as number | null;
+        if (val == 0) {
+          val = null;
         }
-        const htmlGrid = [];
-        for (let row = 0; row < gridSize; row++) {
-            const rowCells = [];
-            for (let col = 0; col < gridSize; col++) {
-                let val = sudokuGrid[row][col].value as number | null;
-                if (val == 0) {
-                    val = null
-                }
-                rowCells.push(
-                    <TouchableOpacity
-                        key={`cell-${row}-${col}`}
-                        style={getCellClass(row, col)}
-                        onPress={() => props.onCellClick(row, col)}
-                    >
-                        <Text>{val}</Text>
-                    </TouchableOpacity>
-                );
+        rowCells.push(
+          <SudokuCell
+            cell={sudokuGrid[row][col]}
+            key={`cell-${row}-${col}`}
+            row={row}
+            col={col}
+            isSelected={
+              props.selectedCell[0] === row && props.selectedCell[1] === col
             }
-            htmlGrid.push(
-                <View key={`row-${row}`} style={styles.gridRow}>
-                    {rowCells}
-                </View>
-            );
-        }
-        return htmlGrid;
+            onCellClick={props.onCellClick}
+          />
+        );
+      }
+      htmlGrid.push(
+        <View key={`row-${row}`} style={styles.gridRow}>
+          {rowCells}
+        </View>
+      );
     }
+    return htmlGrid;
+  }
 
-    function getCellClass(row: number, col: number) {
-        const selectedStyles: [object] = [styles.gridCell]
-        if (row === props.selectedCell[0] && col === props.selectedCell[1]) {
-            selectedStyles.push(styles.selectedCell);
-        }
-        if (props.grid!![row][col].isMarked) {
-            selectedStyles.push(styles.markedCell);
-        }
-        return selectedStyles;
-    }
-
-    return <View style={styles.gridContainer}>{htmlGrid}</View>;
+  return <View style={styles.gridContainer}>{htmlGrid}</View>;
 }
 
 const styles = StyleSheet.create({
-    gridContainer: {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-    },
-    gridRow: {
-        flexDirection: "row",
-    },
-    gridCell: {
-        width: 30,
-        height: 30,
-        borderWidth: 1,
-        borderColor: "gray",
-    },
-    selectedCell: {
-        backgroundColor: "red",
-    },
-    markedCell: {
-        backgroundColor: "green",
-    }
+  gridContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+  gridRow: {
+    flexDirection: "row",
+  },
 });
